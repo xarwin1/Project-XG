9fb1347507c5f59e995b97e9c1ff810d178763bf
Xarwin Gabriel <xarwin.gabriel@protonmail.ch>
2023-03-23 20:36:35 +0800

    added poweroff command

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import creds
 import discord
 from discord.ext import commands
+from colorama import Back, Fore, Style
+import time
+import platform
+
 
 
 client = commands.Bot(command_prefix= "$", intents=discord.Intents.all())
@@ -10,7 +14,9 @@ client = commands.Bot(command_prefix= "$", intents=discord.Intents.all())
 @client.event
 
 async def on_ready():
-
+    prfx = (Back.BLACK + Fore.BLUE + time.strftime("%H:%M:%S GMT+8", time.gmtime()
+    ) + Back.RESET + Fore.WHITE + Style.BRIGHT)
+    print(prfx + " Logged in as " + Fore.BLUE + client.user.name)
     print("Bot is now up!!!")
     
 
@@ -18,6 +24,14 @@ async def on_ready():
 
 async def test(ctx):
     await ctx.send("Command is working papa")
+
+
+@client.command()
+
+async def poweroff(ctx):
+    await ctx.send("Turning off...")
+    await ctx.send("Goodbye")
+    await client.close()
 
 
 
