4e318c69f8a0ef3bf5d3fc60a0b0ec0abd105081
Xarwin Gabriel <xarwin.gabriel@protonmail.ch>
2023-03-23 20:00:20 +0800

    hid the login token

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
+import creds
 import discord
 from discord.ext import commands
 
 
+
+
+
 client = commands.Bot(command_prefix= "$", intents=discord.Intents.all())
-login_token = "MTA4Nzk0NDY3Mzk5NTI3MjIzMg.G5ZJ7G.5szT9fyd6Q3090E2-IpJK3PSgRtBXZLe_HHzD0"
+
 
 
 @client.event
@@ -18,4 +22,6 @@ async def on_ready():
 async def test(ctx):
     await ctx.send("Command is working papa")
 
-client.run(login_token)
+
+
+client.run(creds.login_token)
