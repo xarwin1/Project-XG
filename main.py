5bf65c03bfe13fd8c71cc3d91e2f9d42418cc47f
Xarwin Gabriel <xarwin.gabriel@protonmail.ch>
2023-03-23 12:00:28 +0800

    made a simple command for the bot to respond

--- main.py
+++ main.py
@@ -1,4 +1,21 @@
-name = "xarwin"
+import discord
+from discord.ext import commands
 
-print(name)
 
+client = commands.Bot(command_prefix= "$", intents=discord.Intents.all())
+login_token = "MTA4Nzk0NDY3Mzk5NTI3MjIzMg.G5ZJ7G.5szT9fyd6Q3090E2-IpJK3PSgRtBXZLe_HHzD0"
+
+
+@client.event
+
+async def on_ready():
+
+    print("Bot is now up!!!")
+    
+
+@client.command()
+
+async def test(ctx):
+    await ctx.send("Command is working papa")
+
+client.run(login_token)
