b0a155601873ed61e5a8bd48ade7b55f3db771cb
Xarwin Gabriel Gloria <xarwin.gabriel@protonmail.ch>
2023-04-13 21:29:14 +0800

    added play for youtube command

--- cogs/youtube_music.py
+++ cogs/youtube_music.py
@@ -18,18 +18,26 @@ class youtube(commands.Cog):
         self.client = client
 
     @commands.command()
-    async def play(self, ctx, client, url):
+    async def play(self, ctx, url):
 
         
+        
         channel = ctx.message.author.voice.channel
-        voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
+        voice = discord.utils.get(self.client.voice_clients, guild=ctx.guild)
 
         loop = asyncio.get_event_loop()
         data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download=False))
         song = data['url']
         player = discord.FFmpegPCMAudio(song, **ffmpeg_options)
 
+        if voice is None or not voice.is_connected():
+            voice = await channel.connect()
         voice.play(player)
+            
+        
+
+
+
 
 async def setup(client):
     await client.add_cog(youtube(client))
